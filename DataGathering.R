47bb35b0b505c87835fa49cdb01a46d187120632
Raffi Gharakhanian <raffig@uw.edu>
2017-05-24 19:52:15 -0700

    Updated DataGathering

--- DataGathering.R
+++ DataGathering.R
@@ -1,16 +1,9 @@
 library(dplyr)
 
-official.names.data <- read.csv('data/NFLUniversities.csv')
-lat.long.data <- read.csv('data/UniversitiesLatLong.csv')
+official.names.data <- read.csv('data/NFLUniversities.csv', stringsAsFactors = FALSE)
+lat.long.data <- read.csv('data/UniversitiesLatLong.csv', stringsAsFactors = FALSE)
 colnames(lat.long.data)[1] <- 'Official.Name'
-official.lat.long <- full_join(official.names.data, lat.long.data, by='Official.Name')
+official.lat.long <- full_join(official.names.data, lat.long.data, by='Official.Name') %>% filter(College.Univ != '')
 official.draft.data <- read.csv('data/nfl_draft.csv')
-
-CombineData <- function(univ.name) {
-  draft.data <- filter(official.draft.data, College.Univ == univ.name)
-  lat.long.data <- filter(official.lat.long, College.Univ == univ.name)
-  final.data <- full_join(draft.data, lat.long.data, by='College.Univ')
-}
-
-full.draft.data <- lapply(as.character(official.draft.data$College.Univ), CombineData) %>% bind_rows()
+full.draft.data <- full_join(official.draft.data, official.lat.long, by='College.Univ')
 write.csv(full.draft.data, file = 'Final_Draft_Data.csv', row.names = FALSE)
