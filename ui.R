84ae2c4b27f659c96b04127f96d68461eee192a3
Michelle <michho@uw.edu>
2017-05-24 17:57:57 -0700

    updated ui.R to contain skeleton of code

--- ui.R
+++ ui.R
@@ -1,1 +1,22 @@
+# INFO 201 Group Final Project
+# Michelle Ho, Raffi Gharakhanian, Jon Cantle, Josh Dugger
+
 library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Title"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = ,
+                  label = ,
+                  choices = c(),
+                  selected = "All")
+    ),
+    
+    mainPanel(
+      
+    )
+    
+  )
+))
