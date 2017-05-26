0dc43f61aa77412d920a6ae820ade5cae8d4f799
Raffi Gharakhanian <raffig@uw.edu>
2017-05-26 16:06:27 -0700

    Initial Scatter Plot

--- RaffiMichelle/RaffiMichelle/server.R
+++ RaffiMichelle/RaffiMichelle/server.R
@@ -1,11 +1,25 @@
 library(shiny)
-shinyServer(function(input, output) {
-  
-  #Aggregating similar positions
-  LB <- c('LB', 'MLB', 'ILB', 'OLB') #make all values equal to LB for calculations
-  S <- c('S', 'FS', 'SS') #make all values equal to S for calculations
-  CB <- c('CB', 'DB') #make all values equal to CB for calculations
-  
+
+  shinyServer(function(input, output) {
+    output$statPlot <- renderPlotly({
+      
+      cumulative.stat.data <- draft.data %>% filter(draft.data$GenPos == input$GenPos)
+      
+      StatAverageByYear <- function(year) {
+        plot.data <- cumulative.stat.data %>% filter(Year == year & !is.na(input$Statistic)) %>% select_(input$Statistic)
+        stat.ave <- mean(as.numeric(plot.data[1,]))
+        stat.ave <- round(stat.ave, digits = 2)
+        data.plot <- data.frame(Year = year, Stat = stat.ave)
+        return(data.plot)
+      }
+      
+      plot.data <- lapply(cumulative.stat.data$Year, StatAverageByYear) %>% bind_rows()
+      plot.data <- unique(plot.data)
+      
+      plot_ly(plot.data, x = ~Year, y = ~Stat, name = "Statistics Plot", type='scatter') %>% 
+        add_trace(mode = "markers")
+    })
+  })
   
   # scatterplot: x axis = set of years from 1985-2015 (30 pts for each statistic)
   # user selects position and statistic
@@ -13,18 +27,18 @@ shinyServer(function(input, output) {
   # calculate average of statistic per year
   # y axis = statistic 
   
-  unique.years <- unique(draft.data$Year)
+  #unique.years <- unique(draft.data$Year)
   
-  GetYearAvg <- function(pos.type, pos, stat, year) {
-    year.data <- draft.data %>%
-      filter(Team == pos.type, GenPos == pos, stat >= 0) %>%
-      group_by(Year) %>%
-      arrange(Year) %>% 
-      filter(Year == year) %>%
-      summarise(mean = mean(Def_Int))
-    return (year.data$mean)
+  #GetYearAvg <- function(pos.type, pos, stat, year) {
+   # year.data <- draft.data %>%
+    #  filter(Team == pos.type, GenPos == pos, stat >= 0) %>%
+     # group_by(Year) %>%
+      #arrange(Year) %>% 
+      #filter(Year == year) %>%
+      #summarise(mean = mean(Def_Int))
+    #return (year.data$mean)
     
-  }
+  #}
   
   
-})
+#})
--- RaffiMichelle/RaffiMichelle/ui.R
+++ RaffiMichelle/RaffiMichelle/ui.R
@@ -12,21 +12,21 @@ shinyUI(fluidPage(
       #If the Position Type is Offense and Position is QB
       conditionalPanel(
         condition = "input.Team == 'Offense'", 
-        selectInput(inputId = 'Pos', 'Position:', choices = c('Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR', 'Fullback' = 'FB'), selected = 'Quarterback'),
+        selectInput(inputId = 'GenPos', 'Position:', choices = c('Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR', 'Fullback' = 'FB'), selected = 'Quarterback'),
         conditionalPanel(
-          condition = "input.Pos == 'QB'",
+          condition = "input.GenPos == 'QB'",
           selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Pass Completions' = 'Cmp', 'Pass Attempts' = 'Pass_Att', 'Passing Yards' = 'Pass_Yds', 'Pass Interceptions' = 'Pass_Int', 'Rush Attempts' = 'Rush_Att', 'Rushing Yards' = 'Rush_Yds', 'Rushing Touchdowns' = 'Rush_Tds'), 
                       selected = 'Passing Yards')
         ),
         #Changes statistics if Position is RB, FB, or WR
         conditionalPanel(
-          condition = "input.Pos == 'RB' | input.Pos == 'FB' | input.Pos == 'WR'",
+          condition = "input.GenPos == 'RB' | input.GenPos == 'FB' | input.GenPos == 'WR'",
           selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Rush Attempts' = 'Rush_Att', 'Rushing Yards' = 'Rush_Yds', 'Rushing Touchdowns' = 'Rush_Tds', 'Receptions' = 'Rec', 'Receiving Yards' = 'Rec_Yds', 'Receiving Touchdowns' = 'Rec_Tds'), 
                       selected = 'Rushing Yards')
         ),
         #Changes statistics if Position is TE
         conditionalPanel(
-          condition = "input.Pos == 'TE'",
+          condition = "input.GenPos == 'TE'",
           selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Receptions' = 'Rec', 'Receiving Yards' = 'Rec_Yds', 'Receiving Touchdowns' = 'Rec_Tds'), 
                       selected = 'Receiving Yards')
         )
@@ -34,7 +34,7 @@ shinyUI(fluidPage(
       #Changes Statistics of Position Type is Defense and adds Positions and Statistics since all stats are the same for these positions
       conditionalPanel(
         condition = "input.Team == 'Defense'",
-        selectInput(inputId = 'P', 'Position:', choices = c('Safety' = 'S', 'Cornerback' = 'CB', 'Defensive End' = 'DE', 'Linebacker' = 'LB', 'Defensive Tackle' = 'NT'), 
+        selectInput(inputId = 'GenPos', 'Position:', choices = c('Safety' = 'S', 'Cornerback' = 'CB', 'Defensive End' = 'DE', 'Linebacker' = 'LB', 'Defensive Tackle' = 'NT'), 
                     selected = 'Cornerback'),
         selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Tackles' = 'Tkl', 'Defensive Interceptions' = 'Def_Int', 'Sacks' = 'Sk'), selected = 'Tackles')
       ),
@@ -42,12 +42,7 @@ shinyUI(fluidPage(
       helpText('Any notes we need to put about the selections')
     ),
     mainPanel(
-      
+      plotlyOutput("statPlot")
     )
   )
 ))
-
-#conditionalPanel(
- # condition = "input.Pos == 'S'", #| input.Pos == 'CB' | input.Pos == 'DE' | input.Pos == 'LB' | input.Pos == 'NT'",
- # selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Tackles' = 'Tkl', 'Defensive Interceptions' = 'Def_Int', 'Sacks' = 'Sk'), selected = 'Tackles')
-#),
