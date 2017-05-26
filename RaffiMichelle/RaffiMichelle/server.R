72c0cde4b8f0b008c0f698991b0098092ba80f5a
Raffi Gharakhanian <raffig@uw.edu>
2017-05-25 19:47:29 -0700

    Merged Changes to resolve conflict

--- RaffiMichelle/RaffiMichelle/server.R
+++ RaffiMichelle/RaffiMichelle/server.R
@@ -2,9 +2,9 @@ library(shiny)
 shinyServer(function(input, output) {
   
   #Aggregating similar positions
-  LB <- c('LB', 'MLB', 'ILB', 'OLB')
-  S <- c('S', 'FS', 'SS')
-  OL <- c('OL', 'C', 'G', 'NT', 'T')
+  LB <- c('LB', 'MLB', 'ILB', 'OLB') #make all values equal to LB for calculations
+  S <- c('S', 'FS', 'SS') #make all values equal to S for calculations
+  CB <- c('CB', 'DB') #make all values equal to CB for calculations
   
   
   
--- RaffiMichelle/RaffiMichelle/ui.R
+++ RaffiMichelle/RaffiMichelle/ui.R
@@ -7,25 +7,55 @@ shinyUI(fluidPage(
   sidebarLayout(      
     
     sidebarPanel(
+<<<<<<< HEAD
       selectInput(inputId = "Team", "Position Type:", 
                   choices = c('Offense' = 'Offense', 'Defense' = 'Defense', 'Special Teams' = 'Special Teams')),
       #Panel for Offense
+=======
+      #Select Position Type
+      selectInput(inputId = "Team", "Position Type:", choices = c('Offense' = 'Offense', 'Defense' = 'Defense'), selected = 'Offense'),
+      #If the Position Type is Offense and Position is QB
+>>>>>>> Completed Scatter Plot UI
       conditionalPanel(
-        condition = 'input.Offense == true', 
-        selectInput(inputId = 'Pos', 'Offensive Line' = c('OL', 'C', 'G', 'NT', 'T'), 'Linebacker' = c('LB', 'MLB', 'ILB', 'OLB'), 'Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR'), 
-        selectInput(inputId = 'Statistic', '') #add statistics for Offense
+        condition = "input.Team == 'Offense'", 
+        selectInput(inputId = 'Pos', 'Position:', choices = c('Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR', 'Fullback' = 'FB'), selected = 'Quarterback'),
+        conditionalPanel(
+          condition = "input.Pos == 'QB'",
+          selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Pass Completions' = 'Cmp', 'Pass Attempts' = 'Pass_Att', 'Passing Yards' = 'Pass_Yds', 'Pass Interceptions' = 'Pass_Int', 'Rush Attempts' = 'Rush_Att', 'Rushing Yards' = 'Rush_Yds', 'Rushing Touchdowns' = 'Rush_Tds'), selected = 'Passing Yards')
+        ),
+        #Changes statistics if Position is RB, FB, or WR
+        conditionalPanel(
+          condition = "input.Pos == 'RB' | input.Pos == 'FB' | input.Pos == 'WR'",
+          selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Rush Attempts' = 'Rush_Att', 'Rushing Yards' = 'Rush_Yds', 'Rushing Touchdowns' = 'Rush_Tds', 'Receptions' = 'Rec', 'Receiving Yards' = 'Rec_Yds', 'Receiving Touchdowns' = 'Rec_Tds'), selected = 'Rushing Yards')
+        ),
+        #Changes statistics if Position is TE
+        conditionalPanel(
+          condition = "input.Pos == 'TE'",
+          selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Receptions' = 'Rec', 'Receiving Yards' = 'Rec_Yds', 'Receiving Touchdowns' = 'Rec_Tds'), selected = 'Receiving Yards')
         )
-      #Create Conditional Panel for Defense and Special Teams
       ),
+<<<<<<< HEAD
       selectInput(inputId = "Pos", "Position:", 
                   choices = c('Safety' = c('S', 'FS', 'SS'), 'Cornerback' = 'CB', 'Defensive Back' = 'DB', 'Defensive End' = 'DE', 'Fullback' = 'FB', 'Free Safety' = 'FS', 'Kicker' = 'K', 'Return Specialist' = 'KR', 'Punter' = 'P'),
+=======
+      #Changes Statistics of Position Type is Defense and adds Positions and Statistics since all stats are the same for these positions
+      conditionalPanel(
+        condition = "input.Team == 'Defense'",
+        selectInput(inputId = 'P', 'Position:', choices = c('Safety' = 'S', 'Cornerback' = 'CB', 'Defensive End' = 'DE', 'Linebacker' = 'LB', 'Defensive Tackle' = 'NT'), selected = 'Cornerback'),
+        selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Tackles' = 'Tkl', 'Defensive Interceptions' = 'Def_Int', 'Sacks' = 'Sk'), selected = 'Tackles')
+      ),
+     
+>>>>>>> Completed Scatter Plot UI
       hr(),
       helpText('Any notes we need to put about the selections')
     ),
-    
     mainPanel(
-      plotlyOutput("Plot")  
+      
     )
-    
   )
 ))
+
+#conditionalPanel(
+ # condition = "input.Pos == 'S'", #| input.Pos == 'CB' | input.Pos == 'DE' | input.Pos == 'LB' | input.Pos == 'NT'",
+ # selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Tackles' = 'Tkl', 'Defensive Interceptions' = 'Def_Int', 'Sacks' = 'Sk'), selected = 'Tackles')
+#),
