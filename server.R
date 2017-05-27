830865f5ae0f35238138961b9b7e15825830658f
Joshua Dugger <duggerj@Joshuas-MacBook-Air.local>
2017-05-26 20:22:38 -0700

    Added UI for map. Renders leaflet plot and widgets.

--- server.R
+++ server.R
@@ -1,52 +1,55 @@
-library(dplyr)
 library(shiny)
-library(leaflet)
-library(stringr)
 
-nfl.data <- read.csv("Data/Final_Draft_Data.csv", stringsAsFactors = FALSE)
-all.lat.long <- read.csv("Data/UniversitiesLatLong.csv", stringsAsFactors = FALSE) 
-
-names(all.lat.long) <- c("Official.Name", "LONGITUDE", "LATITUDE")
-all.nfl.colleges <- read.csv("Data/NFLUniversities.csv", stringsAsFactors = FALSE)
-
-individual.colleges <- left_join(x = all.lat.long, y = all.nfl.colleges, by = "Official.Name") %>% 
-  filter(!is.na(College.Univ)) 
-
-nfl.map.data <- nfl.data %>% 
-  select(Year, Rnd, Pos, Pos, Official.Name, LONGITUD, LATITUDE) %>% 
-  filter(Year >= 2005)
-
-count.player.per.college <- nfl.data %>% 
-  count(Official.Name)
-
-final.college.data <- left_join(x = individual.colleges, y = count.player.per.college, by = "Official.Name")
-
-
-
-nfl.map<- final.college.data %>%
-  leaflet() %>%
-  addTiles() %>%
-  setView(lng = -114.805089, lat = 35.3327636, zoom = 3) %>%
-  addMarkers(clusterOptions = markerClusterOptions(iconCreateFunction=JS(
-    iconCreateFunction=JS("function (cluster) {    
-                          var childCount = cluster.getChildCount(); 
-                          var c = ' marker-cluster-';  
-                          c += 'small';  
-                          return new L.DivIcon({ html: 
-                          '<div><span>' + childCount + '</span></div>', 
-                          className: 'marker-cluster' + c, iconSize: new 
-                          L.Point(40, 40) });}"))),
+# Define server logic for slider examples
+shinyServer(function(input, output) {
+ 
+  # Show the values using an HTML table
+  output$map <- renderLeaflet({
+    library(dplyr)
+    library(shiny)
+    library(leaflet)
+    library(stringr)
     
-    label = paste0(strwrap(final.college.data$Official.Name), ": ", strwrap(final.college.data$n), " NFL players drafted") ,
+    nfl.data <- read.csv("Data/Final_Draft_Data.csv", stringsAsFactors = FALSE)
+    all.lat.long <- read.csv("Data/UniversitiesLatLong.csv", stringsAsFactors = FALSE)
     
-    lng = (as.numeric(final.college.data$LONGITUDE)), lat = (as.numeric(final.college.data$LATITUDE)), icon = makeIcon(
-      iconUrl = final.college.data$X.1,
-      iconWidth = 40, iconHeight = 30)) %>% 
-  addTiles()
+    names(all.lat.long) <- c("Official.Name", "LONGITUDE", "LATITUDE")
+    all.nfl.colleges <- read.csv("Data/NFLUniversities.csv", stringsAsFactors = FALSE)
+    
+    individual.colleges <- left_join(x = all.lat.long, y = all.nfl.colleges, by = "Official.Name") %>%
+      filter(!is.na(College.Univ))
+    
+    nfl.map.data <- nfl.data %>%
+      select(Year, Rnd, Pos, Pos, Official.Name, LONGITUD, LATITUDE) %>%
+      filter(Year >= 2005)
+    
+    count.player.per.college <- nfl.data %>%
+      count(Official.Name)
+    
+    final.college.data <- left_join(x = individual.colleges, y = count.player.per.college, by = "Official.Name")
+    
+    
+    
+    nfl.map<- final.college.data %>%
+      leaflet() %>%
+      addTiles() %>%
+      setView(lng = -114.805089, lat = 35.3327636, zoom = 3) %>%
+      addMarkers(clusterOptions = markerClusterOptions(iconCreateFunction=JS(
+        iconCreateFunction=JS("function (cluster) {
+                              var childCount = cluster.getChildCount();
+                              var c = ' marker-cluster-';
+                              c += 'small';
+                              return new L.DivIcon({ html:
+                              '<div><span>' + childCount + '</span></div>',
+                              className: 'marker-cluster' + c, iconSize: new
+                              L.Point(40, 40) });}"))),
+        
+        label = paste0(strwrap(final.college.data$Official.Name), ": ", strwrap(final.college.data$n), " NFL players drafted") ,
+        
+        lng = (as.numeric(final.college.data$LONGITUDE)), lat = (as.numeric(final.college.data$LATITUDE)), icon = makeIcon(
+          iconUrl = final.college.data$X.1,
+          iconWidth = 40, iconHeight = 30)) %>%
+      addTiles()
+  })
+})
 
-nfl.map
-library(dplyr)
-library(shiny)
-library(leaflet)
-
-nfl.data 
--- ui.R
+++ ui.R
@@ -1,22 +1,42 @@
 # INFO 201 Group Final Project
 # Michelle Ho, Raffi Gharakhanian, Jon Cantle, Josh Dugger
-
 library(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("Title"),
+shinyUI(fluidPage(    
   
-  sidebarLayout(
+  titlePanel(h1("Where Do Draft Picks Come From?", align = 'center')),
+  
+  sidebarLayout(      
+    
     sidebarPanel(
-      selectInput(inputId = ,
-                  label = ,
-                  choices = c(),
-                  selected = "All")
+      sliderInput(inputId = "Year", "Choose the Year(s)",
+                  min = 2000, max = 2015, value = c(2000, 2015), sep = ""),
+      #Select Team Type
+      selectInput(inputId = "Team", "Position Type:", 
+                  choices = c('Offense' = 'Offense', 'Defense' = 'Defense'), 
+                  selected = 'Offense'),
+      #If the Position Type is Offense, select offensive positions
+      conditionalPanel(
+        condition = "input.Team == 'Offense'", 
+        selectInput(inputId = 'Pos', 'Position:', 
+                    choices = c('Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR', 'Fullback' = 'FB'), 
+                    selected = 'Quarterback')
+      ),
+      #If the Position Type is Defense, select defensive positions      
+      conditionalPanel(
+        condition = "input.Team == 'Defense'",
+        selectInput(inputId = 'P', 'Position:', 
+                    choices = c('Safety' = 'S', 'Cornerback' = 'CB', 'Defensive End' = 'DE', 'Linebacker' = 'LB', 'Defensive Tackle' = 'NT'), 
+                    selected = 'Cornerback')
+      )
     ),
-    
     mainPanel(
-      
+      leafletOutput("map")
     )
-    
   )
 ))
+
+#conditionalPanel(
+# condition = "input.Pos == 'S'", #| input.Pos == 'CB' | input.Pos == 'DE' | input.Pos == 'LB' | input.Pos == 'NT'",
+# selectInput(inputId = 'Statistic', 'Statistic:', choices = c('Tackles' = 'Tkl', 'Defensive Interceptions' = 'Def_Int', 'Sacks' = 'Sk'), selected = 'Tackles')
+#),
