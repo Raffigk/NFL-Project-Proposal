74a461bda41d0f2f02471cde6db60ba3b27e4205
Joshua Dugger <duggerj@Joshuas-MacBook-Air.local>
2017-05-30 13:39:17 -0700

    Changed player search to include all people containing the input. eg (Aaron = all Aaron)

--- server.R
+++ server.R
@@ -11,55 +11,60 @@ shinyServer(function(input, output) {
     player.data <- draft.data  #From OrganizeData, need to write a csv for this.
     
     # data for each individual college
-    college.data <- read.csv("Data/map.college.data.csv", stringsAsFactors = FALSE) 
-    # Filter Year Range
-    player.data <- player.data %>% filter(Year >= input$Year[1] & Year <= input$Year[2])
-    # Filter Round
-    player.data <- player.data %>% filter(player.data$Rnd >= input$round[1] & player.data$Rnd <= input$round[2])
-    
-    #filters out the player that the user enters.
-    if (input$player != "") {
-      player.data <- player.data %>% filter(player.data$Player == input$player)
-    }# error 2 cases: peyton manning(idk why not working), ike taylor(guy with no university)
-    
-    
-     if (input$Team != 'Both'){
-      player.data <- player.data %>% filter(Team == input$Team)
-     # player.data <- player.data %>% filter(Pos == input$Pos)
-     }
-    
-    #player.data <- player.data %>% filter(Pos == "RB")
-    map.college.data <- college.data %>% filter(Official.Name %in% player.data$Official.Name)
-    
-    
-    
-    
-    if (input$round[1] == input$round[2]) {
-      round.string <- paste0(" in round ", input$round[1])
-    } else {
-      round.string <- paste0(" between rounds ", input$round[1], " and ", input$round[2])
+    college.data <- read.csv("Data/map.college.data.csv", stringsAsFactors = FALSE)
+    if (input$player != ""){
+      player.data <- player.data %>% filter(grepl(input$player, Player))
+      View(player.data)
+    } else{
+      # Filter Year Range
+      player.data <- player.data %>% filter(Year >= input$Year[1] & Year <= input$Year[2])
+      # Filter Round
+      player.data <- player.data %>% filter(Rnd >= input$round[1] & Rnd <= input$round[2])
+      
+      ### Attempt to filter by team and pos inputs ---- Only filters with team, does not work with Position ####
+      if (input$Team != 'Both'){
+        player.data <- player.data %>% filter(Team == input$Team)
+        if (input$Pos != 'All'){
+          player.data <- player.data %>% filter(Pos == input$Pos)
+        }
+      }   
     }
     
-    if (input$Year[1] == input$Year[2]) {
+    # Filters colleges to be ploted to only the colleges with draft picks meeting the criteria above
+    map.college.data <- college.data %>% filter(Official.Name %in% player.data$Official.Name)
+    
+    # Construct the labeling
+    # One round versus multiple
+    if (input$round[1] == input$round[2]) { # One round
+      round.string <- paste0(" in round ", input$round[1])
+    } else { # Multiple rounds
+      round.string <- paste0(" between rounds ", input$round[1], " and ", input$round[2])
+    }
+    # One year versus multiple
+    if (input$Year[1] == input$Year[2]) { # one year
       year.string <- paste0(" In the year ", input$Year[1], ", ")
-    } else {
+    } else { # multiple years
       year.string <- paste0("Between the years ", input$Year[1], " and ", input$Year[2], ", ")
     }
     
+    #### Josh needs to be explained what is going on here ####
     tester.data <- player.data %>% filter(Official.Name == map.college.data$Official.Name)
     tester.num <- nrow(tester.data) # wrong
     
-    if (input$player == "") {
-    college.stats <-  paste0(year.string, tester.num ," ", input$Pos,
-                             "'s drafted", round.string)
-    } else {
-      college.stats <- paste0("In ", player.data$Year, ", ", input$player, " was drafted by ", player.data$Tm, ". He was picked in round ",
+    # Stats for the college label, how many picks during the year and what position.
+    if (input$player == "" && input$Pos != "All") {
+      college.stats <-  paste0(year.string, tester.num ," ", input$Pos,
+                             "'s were drafted", round.string)
+    } else if (input$player == "" && input$Pos == "All"){  # If position is All, change to "players"
+      college.stats <- paste0(year.string, tester.num , " players's were drafted", round.string)
+    } else{ # If single player is searched for
+      college.stats <- paste0("In ", player.data$Year, ", ", player.data$Player, " was drafted by ", player.data$Tm, ". He was picked in round ",
                               player.data$Rnd, " (", player.data$Pick, " overall)")
     }
-    
+    # Apply line break to labels
     labs = paste0(map.college.data$Official.Name, '<br />', 
                    college.stats)
-    
+    # render map
     nfl.map<- map.college.data %>%
       leaflet() %>%
       addTiles() %>%
@@ -74,21 +79,13 @@ shinyServer(function(input, output) {
                               '<div><span>' + childCount + '</span></div>', 
                               className: 'marker-cluster' + c, iconSize: new 
                               L.Point(40, 40)});}"))),
-        
-        label = lapply(labs, HTML),
-        
-       #label = paste0(strwrap(map.college.data$Official.Name),": ", college.stats),
-       #label = htmltools::HTML(paste0(map.college.data$Official.Name,":<br/>", college.stats)), 
-       #This is my attempt to make multiple lines^, grrr why doesnt it work, doesnt look at map.coll.ege.data$Official.Name like the map normally does.
-        labelOptions = labelOptions(direction = 'top', style = list()),
-        
-        
-        lng = (as.numeric(map.college.data$LONGITUDE)), lat = (as.numeric(map.college.data$LATITUDE)), icon = makeIcon(
-          iconUrl = map.college.data$X.1,
-          iconWidth = 50, iconHeight = 40)) %>% 
+      label = lapply(labs, HTML),
+      labelOptions = labelOptions(direction = 'top', style = list()),
+      lng = (as.numeric(map.college.data$LONGITUDE)), lat = (as.numeric(map.college.data$LATITUDE)), icon = makeIcon(
+        iconUrl = map.college.data$X.1,
+        iconWidth = 50, iconHeight = 40)) %>% 
       addTiles()
-    
     nfl.map
-})
   })
+})
 
--- ui.R
+++ ui.R
@@ -6,122 +6,116 @@ library(leaflet)
 library(shinythemes)
 library(dplyr)
 
-shinyUI(fluidPage(theme = shinytheme("superhero"), 
-                  
-                  tabsetPanel(
-                    
-                    # gives a summary of our data collection and what we will be presenting.
-                    tabPanel("Intro Page",
-                             titlePanel("NFL statistics"),
-                             h4("Michelle Ho, Raffi Gharakhanian, Jon Cantle, Josh Dugger"),
-                             h3("The Data"),
-                               p("We discovered a data set containing information on NFL
-                                 players drafted from the years 1985 to 2015, ",
-                                 a("here(ERROR)", href = "https://www.kaggle.com/ronaldjgrafjr/nfl-draft-outcome"),
-                                 ". This dataset was originally created by Ron Graf, who collected the data from ",
-                                 a("Pro Football Reference,", href = "http://www.pro-football-reference.com/"),
-                                 " a NFL players statistics website."), 
-                               p("In addition to that data set, we will also be using a data set that gives
-                                 us information about the location of all colleges in the United States, found ",
-                                 a("here", href ="https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx"),
-                                 "(the data set titled
-                                 “Directory Information(cant find this, need better directions”). We will be using
-                                 this data set to map each college that players in the NFL played at."),
-                             h3("Inspirations to look deeper"),
-                               p("With the NFL being such a popular sport, there were plenty of unanswered 
-                                 questions that we found could be answered through our data sets."),
-                               p("Some questions we believe could be answered and could provide valuable results include:"),
-                               p("- Which colleges produce the best NFL players (based on which round they were picked)
-                                 for each position?"),
-                               p("- Is there a relationship between the college location and strength of position for
-                                 a particular player?"),
-                               p("- What different characteristics (college, location, round picked in, year picked"),
-                               p("- How the year the player was picked, and the years they played in,
-                                 affect the statistics the player has throughout their career?"),
-                             h3("Displaying our Findings/ Our Data Visualizations:"),
-                             h4("Map of colleges"),
-                               p("We created a map of all the colleges in the United States that had players drafted from
-                                 in the years 2000 - 2015. Our aim was to see visible differences in which colleges showed up 
-                                when we filtered our data by different factors. These factors included the range of rounds 
-                                 picked, the range of years picked, and the position players were getting drafted for."),
-                               p("We were expecting results that could indicate to us which colleges were stronger 
-                                   when it came to supplying players to the NFL, and how the position and the year could change 
-                                 which colleges were stronger (paragraph needs to be worded better) "),
-                             h4("Scatter plot of player statistics"),
-                             p("We had an idea that over the years, the NFL has changed drastically. These changes have resulted
-                               in the game being played differently, resulting in priorities changing, and ultimitely statistics."),
-                             p("We created a scatter plot that would plot the statistics change over the years of the NFL, with
-                               options to choose between position and type of statistic."),
-                             p("We are expecting to see trends either  moving upwards or downwards in a few different statistic groups,
-                               as time moves forward, and we believe we could make sense of some of these statistic changes."),
-                             mainPanel(
-                               verbatimTextOutput("intro page")
-                             )
-                             ),
-                 
-                    # A map comparing location with round/position/year
-                     tabPanel("Map",
-  
-  titlePanel(h1("Where Do Draft Picks Come From?", align = 'center')),
-  
-  sidebarLayout(
-    
-    sidebarPanel(
-      #User input a player name 
-      textInput("player", label = h5("Player select"), value = "", width = NULL,
-                placeholder = "Choose a player"),
-      helpText("For example : Aaron Rodgers"),
-      # submitButton(text = "Enter"), # this is applied to all the widgets, I only want it for the textInput^, FIX!
-  
-   
-      #Slider for year selector
-      sliderInput(inputId = "Year", "Choose the Year(s)",
-                  min = 1985, max = 2015, value = c(1985, 2015), sep = ""),
-      #Slider for round selector
-      sliderInput(inputId = "round", "Choose the Round(s)",
-                  min = 1, max = 12,  value = c(1, 12), sep = ""),
-      #Select Team Type
-      selectInput(inputId = "Team", "Position Type:", 
-                  choices = c('Offense' = 'Offense', 'Defense' = 'Defense', 'Both' = 'Both'),
-                  selected = 'Both'),
-      
-      #If the team type is both, select all positions
-      conditionalPanel(
-        condition = "input.Team == 'Both'",
-        selectInput(inputId = 'Pos', "Position:",
-                    choices = c('All' = 'All'),
-                    selected = 'All')
+shinyUI(
+  fluidPage(
+    theme = shinytheme("superhero"),
+    tabsetPanel(
+      # gives a summary of our data collection and what we will be presenting.
+      tabPanel(
+        "Intro Page",
+        titlePanel("NFL statistics"),
+        h4("Michelle Ho, Raffi Gharakhanian, Jon Cantle, Josh Dugger"),
+        h3("The Data"),
+        p("We discovered a data set containing information on NFL players drafted from the years 1985 to 2015, ",
+          a("here(ERROR)", href = "https://www.kaggle.com/ronaldjgrafjr/nfl-draft-outcome"),
+          ". This dataset was originally created by Ron Graf, who collected the data from ",
+          a("Pro Football Reference,", href = "http://www.pro-football-reference.com/"),
+          " a NFL players statistics website."), 
+        p("In addition to that data set, we will also be using a data set that gives
+          us information about the location of all colleges in the United States, found ",
+          a("here", href ="https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx"),
+          "(the data set titled
+          “Directory Information(cant find this, need better directions”). We will be using
+          this data set to map each college that players in the NFL played at."),
+        h3("Inspirations to look deeper"),
+        p("With the NFL being such a popular sport, there were plenty of unanswered 
+           questions that we found could be answered through our data sets."),
+        p("Some questions we believe could be answered and could provide valuable results include:"),
+        p("- Which colleges produce the best NFL players (based on which round they were picked)
+          for each position?"),
+        p("- Is there a relationship between the college location and strength of position for
+          a particular player?"),
+        p("- What different characteristics (college, location, round picked in, year picked"),
+        p("- How the year the player was picked, and the years they played in,
+          affect the statistics the player has throughout their career?"),
+        h3("Displaying our Findings/ Our Data Visualizations:"),
+        h4("Map of colleges"),
+        p("We created a map of all the colleges in the United States that had players drafted from
+          in the years 2000 - 2015. Our aim was to see visible differences in which colleges showed up 
+          when we filtered our data by different factors. These factors included the range of rounds 
+          picked, the range of years picked, and the position players were getting drafted for."),
+        p("We were expecting results that could indicate to us which colleges were stronger 
+          when it came to supplying players to the NFL, and how the position and the year could change 
+          which colleges were stronger (paragraph needs to be worded better) "),
+        h4("Scatter plot of player statistics"),
+        p("We had an idea that over the years, the NFL has changed drastically. These changes have resulted
+          in the game being played differently, resulting in priorities changing, and ultimitely statistics."),
+        p("We created a scatter plot that would plot the statistics change over the years of the NFL, with
+          options to choose between position and type of statistic."),
+        p("We are expecting to see trends either  moving upwards or downwards in a few different statistic groups,
+          as time moves forward, and we believe we could make sense of some of these statistic changes."),
+        mainPanel(
+          verbatimTextOutput("intro page")
+        )
       ),
-      #If the Position Type is Offense, select offensive positions
-      conditionalPanel(
-        condition = "input.Team == 'Offense'", 
-        selectInput(inputId = 'Pos', 'Position:', 
-                    choices = c('Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR', 'Fullback' = 'FB'), 
-                    selected = 'Quarterback')
-      ),
-      #If the Position Type is Defense, select defensive positions      
-      conditionalPanel(
-        condition = "input.Team == 'Defense'",
-        selectInput(inputId = 'P', 'Position:', 
-                    choices = c('Safety' = 'S', 'Cornerback' = 'CB', 'Defensive End' = 'DE', 'Linebacker' = 'LB', 'Defensive Tackle' = 'NT'), 
-                    selected = 'Cornerback')
-      ),
-      hr(),
-      helpText("Data for years 1993/2003 missing from dataset")
-    ),
-    mainPanel(
-      leafletOutput("map")
+      # A map comparing location with round/position/year
+      tabPanel("Map",
+        titlePanel(h1("Where Do Draft Picks Come From?", align = 'center')),
+        sidebarLayout(
+          sidebarPanel(
+          #User input a player name 
+          textInput("player", label = h5("Player select"), value = "", width = NULL,
+            placeholder = "Choose a player"),
+          helpText("For example : Aaron Rodgers"),
+          hr(),
+          conditionalPanel( condition = "input.player == ''",
+            #Slider for year selector
+            sliderInput(inputId = "Year", "Choose the Year(s)",
+              min = 1985, max = 2015, value = c(1985, 2015), sep = ""),
+            #Slider for round selector
+            sliderInput(inputId = "round", "Choose the Round(s)",
+              min = 1, max = 12,  value = c(1, 12), sep = ""),
+            #Select Team Type
+            selectInput(inputId = "Team", "Position Type:", 
+              choices = c('Offense' = 'Offense', 'Defense' = 'Defense', 'Both' = 'Both'),
+              selected = 'Both'),
+            #If the team type is both, select all positions
+            conditionalPanel(
+              condition = "input.Team == 'Both'",
+              selectInput(inputId = 'Pos', "Position:",
+                choices = c('All' = 'All'),
+                selected = 'All')
+            ),
+            #If the Position Type is Offense, select offensive positions
+            conditionalPanel(
+              condition = "input.Team == 'Offense'", 
+              selectInput(inputId = 'Pos', 'Position:', 
+                choices = c('All' = 'All', 'Quarterback' = 'QB', 'Running Back' = 'RB', 'Tight End' = 'TE', 'Wide Receiver' = 'WR', 'Fullback' = 'FB'), 
+                selected = 'All')
+            ),
+            #If the Position Type is Defense, select defensive positions      
+            conditionalPanel(
+              condition = "input.Team == 'Defense'",
+              selectInput(inputId = 'Pos', 'Position:', 
+                choices = c('All' = 'All', 'Safety' = 'S', 'Cornerback' = 'CB', 'Defensive End' = 'DE', 'Linebacker' = 'LB', 'Defensive Tackle' = 'NT'), 
+                selected = 'All')
+            )
+          ),
+          hr(),
+          helpText("Data for years 1993/2003 missing from dataset")
+        ),
+        mainPanel(
+          leafletOutput("map")
+        )
       )
-    )
     ),
     # Conclusion panel, where seen results are noted.
     tabPanel("Conclusion",
-      
       mainPanel(
         verbatimTextOutput("conclusion"))
+      )
     )
   )
-)
 )
 
 #conditionalPanel(
